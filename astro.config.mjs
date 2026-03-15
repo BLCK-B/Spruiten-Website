1ae12e223bec39a5cb39192f628db7feab66a114
BLCK <123077751+BLCK-B@users.noreply.github.com>
2026-03-15 16:02:46 +0100

    remove cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,48 +7,39 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { SITE } from "./src/config/site.mjs";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
-  site: SITE.url,
-
-  integrations: [
-    react(),
-    icon(),
-    mdx(),
-    sitemap({
-      changefreq: "weekly",
-      priority: 0.7,
-      lastmod: new Date(),
-    }),
-  ],
-
-  vite: {
-    plugins: [tailwindcss()],
-    build: {
-      cssMinify: "lightningcss",
-      minify: "terser",
-      terserOptions: {
-        compress: {
-          drop_console: true,
-          drop_debugger: true,
-        },
-      },
-    },
-  },
-
-  build: {
-    inlineStylesheets: "auto",
-    assets: "_assets",
-  },
-
-  compressHTML: true,
-
-  image: {
-    domains: [],
-    remotePatterns: [],
-  },
-
-  adapter: cloudflare(),
+	site: SITE.url,
+	integrations: [
+		react(),
+		icon(),
+		mdx(),
+		sitemap({
+			changefreq: "weekly",
+			priority: 0.7,
+			lastmod: new Date(),
+		}),
+	],
+	vite: {
+		plugins: [tailwindcss()],
+		build: {
+			cssMinify: "lightningcss",
+			minify: "terser",
+			terserOptions: {
+				compress: {
+					drop_console: true,
+					drop_debugger: true,
+				},
+			},
+		},
+	},
+	build: {
+		inlineStylesheets: "auto",
+		assets: "_assets",
+	},
+	compressHTML: true,
+	image: {
+		domains: [],
+		remotePatterns: [],
+	},
 });
